6d856b404db6ffcdfa5f2a0d2cfc426e29ccfa46
napstaa967 <napstaa967alt@gmail.com>
2022-07-25 18:06:54 -0500

    another correction

--- Project/addons/autogrid/plugin.gd
+++
@@ -1,7 +0,0 @@
-tool
-extends EditorPlugin
-
-func _enter_tree():
-	# Initialization of the plugin goes here.
-	# Add the new type with a name, a parent type, a script and an icon.
-	add_custom_type("AutoGridContainer", "GridContainer", preload("AutoGridContainer.gd"), preload("GridContainer.svg"))
